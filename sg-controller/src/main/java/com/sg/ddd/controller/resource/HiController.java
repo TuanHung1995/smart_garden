937f81d7de6523dd20f941bc051bf490dcd76f20
TuanHung1995 <tuanhungg1995@gmail.com>
2025-05-04 16:08:22 +0700

    circuitbreaker and ratelimiter

--- sg-controller/src/main/java/com/sg/ddd/controller/resource/HiController.java
+++ sg-controller/src/main/java/com/sg/ddd/controller/resource/HiController.java
@@ -1,6 +1,7 @@
 package com.sg.ddd.controller.resource;
 
 import com.sg.ddd.application.service.event.EventAppService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,17 @@ public class HiController {
     private EventAppService eventAppService;
 
     @GetMapping("/hello")
+    @RateLimiter(name = "backendA", fallbackMethod = "helloFallback")
     public String hello() {
+        return eventAppService.sayHi("World");
+    }
+
+    public String helloFallback(Exception e) {
+        return "Too many requests, please try again later.";
+    }
+
+    @GetMapping("/hi-circuit-breaker")
+    public String circuitBreaker() {
         return eventAppService.sayHi("World");
     }
 
