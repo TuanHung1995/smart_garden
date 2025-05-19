c3b999140683d29f1145b32e0243538e3e589dc3
TuanHung1995 <tuanhungg1995@gmail.com>
2025-05-19 15:21:33 +0700

    get local cache for server n'th

--- sg-application/src/main/java/com/sg/ddd/application/service/ticket/impl/TicketDetailAppServiceImpl.java
+++ sg-application/src/main/java/com/sg/ddd/application/service/ticket/impl/TicketDetailAppServiceImpl.java
@@ -22,9 +22,10 @@ public class TicketDetailAppServiceImpl implements TicketDetailAppService {
 
     @Override
     public TicketDetail getTicketDetailById(Long ticketId) {
-        log.info("Implement Application : {}", ticketId);
+//        log.info("Implement Application : {}", ticketId);
 //        return ticketDetailDomainService.getTicketDetailById(ticketId);
 //        return ticketDetailCacheService.getTicketDefaultCacheNormal(ticketId, System.currentTimeMillis());
-        return ticketDetailCacheService.getTicketDefaultCacheVip(ticketId, System.currentTimeMillis());
+//        return ticketDetailCacheService.getTicketDefaultCacheVip(ticketId, System.currentTimeMillis());
+        return ticketDetailCacheService.getTicketDefaultCacheLocal(ticketId, System.currentTimeMillis());
     }
 }
